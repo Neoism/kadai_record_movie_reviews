0d3fa4aad79786acdc0c3e28ac93552601a5f7b5
amiseno <amiseno@agilewarenoMacBook-Pro.local>
2020-09-17 16:36:06 +0900

    add devises

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resouce)
+    root_path
+  end
+
+  protected
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys:[:email, :username])
+      devise_parameter_sanitizer.permit(:sign_in, keys:[:username])
+  end
 end
